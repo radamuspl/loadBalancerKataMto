fd01481234e0b9bd68a7191c45bd36d0c2f30992
Radek Adamus <adamus.r@gmail.com>
2015-02-01 17:28:34 +0100

    zero test itCompiles

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -0,0 +1,16 @@
+package edu.iis.mto.serverloadbalancer;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class ServerLoadBalancerTest {
+	@Test
+	public void itCompiles() {
+		assertThat(true, equalTo(true));
+	}
+
+
+}
