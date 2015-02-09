2f6b7229cc0bd7b95b0c08145808736ca7084602
Radek Adamus <adamus.r@gmail.com>
2015-02-09 09:31:15 +0100

    test 1 refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,10 +1,10 @@
 package edu.iis.mto.serverloadbalancer;
 
-
+import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
+import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -13,43 +13,29 @@ public class ServerLoadBalancerTest {
 		assertThat(true, equalTo(true));
 	}
 
-
 	@Test
-	public void balancingAServer_noVms_serverStaysEmpty(){
+	public void balancingAServer_noVms_serverStaysEmpty() {
 		Server theServer = a(server().withCapacity(1));
-		
+
 		balance(aListOfServersWith(theServer), anEmptyListOfVms());
-		
+
 		assertThat(theServer, hasLoadPercentageOf(0.0d));
 	}
-
-
-	private Matcher<? super Server> hasLoadPercentageOf(double expectedLoadPercentage) {
-		return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
-	}
-
 
 	private void balance(Server[] servers, Vm[] vms) {
 		new ServerLoadBalancer().balance(servers, vms);
 	}
 
-
 	private Vm[] anEmptyListOfVms() {
 		return new Vm[0];
 	}
 
-
 	private Server[] aListOfServersWith(Server server) {
-		return new Server[]{server};
+		return new Server[] { server };
 	}
-
 
 	private Server a(ServerBuilder builder) {
 		return builder.build();
 	}
 
-
-	private ServerBuilder server() {
-		return new ServerBuilder();
-	}
 }
