68c5a6e8dd896404cd6ee601368e3b12d7514fb1
Radek Adamus <adamus.r@gmail.com>
2015-02-13 11:45:00 +0100

    test 6 red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -70,6 +70,15 @@ public class ServerLoadBalancerTest {
 
 	}
 
+	@Test 
+	public void balanceAServerWithNotEnoughRoom_shouldNotBeFilledWithAVm(){
+		Server theServer = a(server().withCapacity(10).withCurrentLoadOf(90.0d));
+		Vm theVm = a(vm().ofSize(2));
+		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+		
+		assertThat("the less loaded server should not contain vm", !theServer.contains(theVm));
+	}
+
 	private void balance(Server[] servers, Vm[] vms) {
 		new ServerLoadBalancer().balance(servers, vms);
 	}
