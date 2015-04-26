9110c363a357306f581013792e5952f5ab3b18a2
Radek Adamus <adamus.r@gmail.com>
2015-04-26 16:17:48 +0200

    class for implementation of parametrized tests added

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
@@ -0,0 +1,22 @@
+package edu.iis.mto.serverloadbalancer;
+
+import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
+import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
+import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest{
+	@Test
+	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
+		Server theServer = a(server().withCapacity(1));
+		Vm theVm = a(vm().ofSize(1));
+		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+		assertThat(theServer, hasLoadPercentageOf(100.0d));
+		assertThat("the server should contain vm", theServer.contains(theVm));
+	}
+	
+	
+}
