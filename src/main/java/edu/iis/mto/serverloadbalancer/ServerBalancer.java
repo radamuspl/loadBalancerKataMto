a86d28b13c6a3211e76a83cf3d9ae6ffda04a39f
Robert <ropak@wp.pl>
2017-06-14 19:35:43 +0200

    * Prawidłowe rozdzielenie maszyn wirtualnych do mniej obciążonych serwerów

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerBalancer.java
@@ -1,8 +1,24 @@
 package edu.iis.mto.serverloadbalancer;
 
 import java.util.Collection;
+import java.util.Comparator;
 
 public class ServerBalancer {
+
+    private static final Comparator<Server> LOAD_COMPARATOR = (s1, s2) -> {
+        double s1Load = s1.getLoad();
+        double s2Load = s2.getLoad();
+
+        double diff = s1Load - s2Load;
+        if(Math.abs(diff) - 0.1 < 0) {
+            return 0;
+        } else if(s1Load < s2Load) {
+            return -1;
+        } else {
+            return 2;
+        }
+    };
+
     public void balance(Collection<Server> servers, Collection<VirtualMachine> virtualMachines) {
         virtualMachines.forEach(virtualMachine -> pickServerAndAddVm(virtualMachine, servers));
     }
@@ -15,6 +31,7 @@ public class ServerBalancer {
     private Server pickServerForVm(VirtualMachine vm, Collection<Server> servers) {
         return servers.stream()
                 .filter(server -> server.canAddVm(vm))
+                .sorted(LOAD_COMPARATOR)
                 .findFirst()
                 .orElse(null);
     }
